bbe654346bf62b6648132dd8ddb80a75e4adeb8a
Ajaykamani <42440464+Ajaykamani@users.noreply.github.com>
2020-09-09 00:26:43 +0530

    Add files via upload

--- SeriesModelService/src/main/java/com/cts/training/SeriesModelService/controller/LoginController.java
+++ SeriesModelService/src/main/java/com/cts/training/SeriesModelService/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //import com.cts.training.SeriesModelService.model.User;
@@ -18,18 +19,24 @@ import com.cts.training.SeriesModelService.repository.UserRepository;
 import com.cts.training.entities.Entities.Models.User;
 import com.cts.training.entities.Entities.Models.UserModel;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+//@RequestMapping("/login")
 @ControllerAdvice
 @RestController
+@Slf4j
 public class LoginController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
 
 	@GetMapping("/login")
-	 public ResponseEntity<UserModel> login(HttpServletRequest request) {
+	//ResponseEntity<UserModel> 
+	public  User login(HttpServletRequest request) {
 		// if called then credentials are valid
+		log.info("iN login controller");
 		String authorization = request.getHeader("Authorization");
 		String[] values = null;
 
@@ -42,9 +49,11 @@ public class LoginController {
 			values = credentials.split(":", 2);
 			}
 		
-		User user = this.userRepository.findByuserName(values[0]).get(0);
-		UserModel data = new UserModel(user.getUserId(),user.getUserName());
-		return new ResponseEntity<UserModel>(data, HttpStatus.OK);
+		User user = this.userRepository.findByuserName(values[0]).get(0);//
+		log.info("login controller"+values[0]);
+		//UserModel data = new UserModel(user.getUserId(),user.getUserName());
+	//	return new ResponseEntity<UserModel>(data, HttpStatus.OK);
+		return user;
 	}
 
 }
--- SeriesModelService/src/main/java/com/cts/training/SeriesModelService/controller/SeriesModelController.java
+++ SeriesModelService/src/main/java/com/cts/training/SeriesModelService/controller/SeriesModelController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 //import com.cts.training.SeriesModelService.model.Series;
 import com.cts.training.SeriesModelService.service.SeriesModelService;
 import com.cts.training.entities.Entities.Models.Models;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import io.swagger.annotations.Api;
 @Api
@@ -21,6 +23,20 @@ public class SeriesModelController {
 	
 	@Autowired
 	private SeriesModelService seriesmodelservice;
+	
+	
+	@Value("${msg:Config Server is not working. Please check...}")
+    private String msg;
+	
+	//@HystrixCommand(fallbackMethod = "fallBackGetMsg" , commandProperties = {
+		   //   @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")})
+    @GetMapping("/msg")
+    public String getMsg() {
+        return this.msg;
+    }
+   // public String fallBackGetMsg() {
+   //     return "fall back method";
+  //  }
 	
 	
 	@GetMapping("/series")
